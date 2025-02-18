190ea80fd532bb14a04a1dac8c8fe8637db6cfd9
vargesz5 <150337962+vargesz5@users.noreply.github.com>
2025-02-18 09:34:46 +0100

    appá alakitás

--- reformApp/src/main.js
+++ reformApp/src/main.js
@@ -1,3 +1,19 @@
+/* Service Worker regisztrálása */
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('/service-worker.js')
+        .then((registration) => {
+          console.log('Service Worker regisztrálva: ', registration);
+        })
+        .catch((error) => {
+          console.log('Service Worker hiba: ', error);
+        });
+    });
+  }
+  
+
 /* Handle Input Fields */
 document.addEventListener("DOMContentLoaded", function () {
     let loginTab = document.getElementById("loginTab");
